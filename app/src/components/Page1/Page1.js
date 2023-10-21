7be51bd5adacb0a5984144b01802cee9ed968012
leastkids <leastkids@gmail.com>
2023-10-21 15:42:23 +0900

    ADD - webpack server

--- app/src/components/Page1/Page1.js
+++ app/src/components/Page1/Page1.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Page2 from '../Page2/Page2'
 import style from './Page1.module.css'
 
+import reactImg from '../../assets/img/img.png'
+
 export default function page1() {
     return (
         <div>
             <h1 className={style.a}>klaj;sdflafjkd;ajklfadfjkladfjkl;</h1>
-            <img src="./image/img.png"/>
+            <img src={reactImg} alt={"test"}/>
+            <div className={"test"}>123</div>
             <Page2 />
         </div>
     )
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'src'),
-        filename: 'bundle.js',
+        filename: './assets/js/index.js',
     },
     module: {
         rules: [
@@ -33,4 +34,24 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // 프로젝트의 HTML 파일 경로
+        }),
+    ],
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'src'), // 이전의 'contentBase'와 동일한 역할을 합니다.
+        },
+        compress: true,
+        port: 9000, // 원하는 포트 번호를 사용합니다.
+        hot: true, // HMR(Hot Module Replacement)을 활성화합니다.
+        open: true, // 서버 실행 시 브라우저를 자동으로 엽니다.
+    },
+    // performance: {
+    //     hints: false, // 이 설정은 성능 힌트 시스템을 완전히 비활성화합니다.
+    // },
+
+
 };
